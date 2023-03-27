3796d69a606e7b9bed7c1cdd67f9d3254409347d
Rubén Pardo <>
2023-03-27 11:31:33 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rudo_app_clone
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/rudo_app_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rudo_app_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
