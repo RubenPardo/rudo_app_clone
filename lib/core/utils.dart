95d58ed90934a1938c42a7cdd4bd5a01bd3f885d
Rubén Pardo <rubenpard@rudo.es>
2023-04-06 13:10:34 +0200

    Fix hours format

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -121,7 +121,6 @@ extension DurationExtension on Duration{
   String toStringHoursMinutes() {
     String twoDigits(int n) => n.toString().padLeft(2, "0");
     String twoDigitMinutes = twoDigits(inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(inSeconds.remainder(60));
-    return "${twoDigits(inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+    return "${twoDigits(inHours)}:$twoDigitMinutes";
   }
 }
